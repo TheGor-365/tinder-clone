4ac7c4944ee5c3a6d6de9d8bd5382a50c1db8bf1
Gor <7495500@gmail.com>
2021-02-11 18:47:46 +0300

    Add basic app configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jpuery',
+    jQuery: 'jquery/src/jpuery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
 module.exports = environment
